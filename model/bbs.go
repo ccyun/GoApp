6e07459248e8b1df47dbbce83a0ca1fffd89c082
guanzheng <zheng.guan@gnetoffice.com>
2016-11-04 18:54:45 +0800

    update

--- model/bbs.go
+++ model/bbs.go
@@ -1,39 +1,36 @@
 package model
 
 import (
-	"log"
+	"encoding/json"
+	"strconv"
 	"time"
 
+	"github.com/astaxie/beego/logs"
 	"github.com/astaxie/beego/orm"
 )
 
 //Bbs 任务表结构
 type Bbs struct {
-	ID                 uint64 `orm:"column(id)"`
-	SiteID             uint64 `orm:"column(site_id)"`
-	BoardID            uint64 `orm:"column(board_id)"`
-	Title              string `orm:"column(title)"`
-	Description        string `orm:"column(description)"`
-	Content            string `orm:"column(content)"`
-	PublishScopeString string `orm:"column(publish_scope)"`
-	MsgCount           uint8  `orm:"column(msg_count)"`
-	Attachments        string `orm:"column(attachments)"`
-	UsesID             uint64 `orm:"column(user_id)"`
-	CreatedAt          uint64 `orm:"column(created_at)"`
-	PublishAt          uint64 `orm:"column(publish_at)"`
-	ModifiedAt         uint64 `orm:"column(modified_at)"`
-	SetTimer           uint64 `orm:"column(set_timer)"`
-	Category           string `orm:"column(category)"`
-	Type               string `orm:"column(type)"`
-	IsDeleted          string `orm:"column(is_deleted)"`
-	Status             uint8  `orm:"column(status)"`
-	CommentEnabled     uint8  `orm:"column(comment_enabled)"`
-}
-
-//BbsInfo 公告
-type BbsInfo struct {
-	Bbs
-	PublishScope map[string][]uint64
+	ID                 uint64              `orm:"column(id)"`
+	SiteID             uint64              `orm:"column(site_id)"`
+	BoardID            uint64              `orm:"column(board_id)"`
+	Title              string              `orm:"column(title)"`
+	Description        string              `orm:"column(description)"`
+	Content            string              `orm:"column(content)"`
+	PublishScopeString string              `orm:"column(publish_scope)"`
+	PublishScope       map[string][]uint64 `orm:"-"`
+	MsgCount           uint8               `orm:"column(msg_count)"`
+	Attachments        string              `orm:"column(attachments)"`
+	UsesID             uint64              `orm:"column(user_id)"`
+	CreatedAt          uint64              `orm:"column(created_at)"`
+	PublishAt          uint64              `orm:"column(publish_at)"`
+	ModifiedAt         uint64              `orm:"column(modified_at)"`
+	SetTimer           uint64              `orm:"column(set_timer)"`
+	Category           string              `orm:"column(category)"`
+	Type               string              `orm:"column(type)"`
+	IsDeleted          string              `orm:"column(is_deleted)"`
+	Status             uint8               `orm:"column(status)"`
+	CommentEnabled     uint8               `orm:"column(comment_enabled)"`
 }
 
 //TableName 表名
@@ -42,12 +39,16 @@ func (B *Bbs) TableName() string {
 }
 
 //GetOne 读取单条数据
-func (B *Bbs) GetOne(ID uint64) (BbsInfo, error) {
+func (B *Bbs) GetOne(ID uint64) (Bbs, error) {
 	bbsInfo := Bbs{}
 	if err := o.QueryTable(B).Filter("ID", ID).One(&bbsInfo); err != nil {
-		return BbsInfo{}, err
+		return Bbs{}, err
 	}
-	return B.afterSelectHandle([]Bbs{bbsInfo})
+	data, err := B.afterSelectHandle([]Bbs{bbsInfo})
+	if err != nil {
+		return Bbs{}, err
+	}
+	return data[0], nil
 }
 
 //Update 修改数据
@@ -60,12 +61,33 @@ func (B *Bbs) Update(ID uint64) error {
 }
 
 //afterSelectHandle 查询结果处理
-func (B *Bbs) afterSelectHandle(data []Bbs) (BbsInfo, error) {
-	log.Println(data)
-
+func (B *Bbs) afterSelectHandle(data []Bbs) ([]Bbs, error) {
+	var (
+		datas []Bbs
+		err   error
+	)
 	for key, item := range data {
-		log.Println(key)
-		log.Println(item)
+		item.PublishScope, err = B.publishScopeHandle(item.PublishScopeString)
+		if err != nil {
+			logs.Error("publishScopeHandle err,bbsID:", item.ID, "PublishScopeString", item.PublishScopeString)
+		}
+		datas[key] = item
 	}
-	return BbsInfo{}, nil
+	return datas, nil
+}
+
+//publishScopeHandle 处理发布范围
+func (B *Bbs) publishScopeHandle(publishScopeString string) (map[string][]uint64, error) {
+	var data map[string][]uint64
+	var publishScope map[string][]string
+	err := json.Unmarshal([]byte(publishScopeString), &publishScope)
+	for k, r := range publishScope {
+		for _, v := range r {
+			id, _ := strconv.Atoi(v)
+			if id > 0 {
+				data[k] = append(data[k], uint64(id))
+			}
+		}
+	}
+	return data, err
 }
