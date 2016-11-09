6b9bfbae81d196d5cdc704da75fc4922fc846ce8
guanzheng <zheng.guan@gnetoffice.com>
2016-11-09 16:49:04 +0800

    update

--- module/vendor/httpcurl/httpcurl_test.go
+++ module/vendor/httpcurl/httpcurl_test.go
@@ -1,11 +1,42 @@
 package httpcurl
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func Test_Request(t *testing.T) {
-
+	UMSBusinessURL = "http://192.168.28.173:8081/umsapi"
+	url := fmt.Sprintf("%s/rs/organizations/query/orgs/users?pageNum=%d&pageSize=%d&productID=%d", UMSBusinessURL, 1, 500, 20)
+	b, _ := json.Marshal([]uint{2752})
+	statusCode, _, err := Request("POST", url, strings.NewReader(string(b)))
+	if err != nil {
+		t.Error(err)
+	}
+	if statusCode != 200 {
+		t.Error(statusCode)
+	}
 }
-func Test_GetAllUserByOrgIDs(t *testing.T) {
+func Test__getAllUserByOrgIDs(t *testing.T) {
+	UMSBusinessURL = "http://192.168.28.173:8081/umsapi"
 	a := new(UMS)
-	a.GetAllUserByOrgIDs([]uint64{1111})
+	_, totalCount, err := a._getAllUserByOrgIDs([]uint64{2752}, 100, 1)
+	if err != nil {
+		t.Error(err)
+	}
+	if totalCount <= 0 {
+		t.Error(totalCount)
+	}
+}
+
+func Test_GetAllUserByOrgIDs(t *testing.T) {
+	UMSBusinessURL = "http://192.168.28.173:8081/umsapi"
+	a := new(UMS)
+	data, err := a.GetAllUserByOrgIDs([]uint64{2752})
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(len(data))
 }
--- module/vendor/httpcurl/ums.go
+++ module/vendor/httpcurl/ums.go
@@ -1,8 +1,15 @@
 package httpcurl
 
 import (
+	"encoding/json"
 	"fmt"
-	"log"
+	"runtime"
+	"strings"
+	"sync"
+
+	"math"
+
+	"github.com/astaxie/beego/logs"
 )
 
 var (
@@ -40,15 +47,51 @@ type UMSOrg struct {
 }
 
 //GetAllUserByOrgIDs 批量获取组织下所有用户
-func (U *UMS) GetAllUserByOrgIDs(orgIDs []uint64) error {
-
-	//$res = $this->ums_api('/rs/organizations/query/orgs/users?pageNum=%s&pageSize=%s&productID=%s', 'post', array($page, $page_size, 20), $org_ids)
-
-	return nil
+func (U *UMS) GetAllUserByOrgIDs(orgIDs []uint64) ([]UMSUser, error) {
+	var pageSize uint64 = 500
+	data, totalCount, err := U._getAllUserByOrgIDs(orgIDs, pageSize, 1)
+	if err != nil {
+		return nil, err
+	}
+	if uint64(len(data)) < totalCount {
+		pageNum := int(math.Ceil(float64(totalCount) / float64(pageSize)))
+		var w sync.WaitGroup
+		runtime.GOMAXPROCS(runtime.NumCPU())
+		for i := 2; i <= pageNum; i++ {
+			w.Add(1)
+			go func(i int) {
+				d, _, _ := U._getAllUserByOrgIDs(orgIDs, pageSize, i)
+				data = append(data, d[0:]...)
+				w.Done()
+			}(i)
+		}
+		w.Wait()
+	}
+	if uint64(len(data)) != totalCount {
+		return nil, fmt.Errorf("GetAllUserByOrgIDs error")
+	}
+	return data, nil
 }
-func (U *UMS) _getAllUserByOrgIDs(orgIDs []uint64, page uint64) ([]UMSUser, uint64, error) {
-	var totalCount uint64
-	url := fmt.Sprintf("%s/rs/organizations/query/orgs/users?pageNum=%d&pageSize=%d&productID=%d", UMSBusinessURL, page, 500, 20)
-	log.Println(url)
-	return nil, totalCount, nil
+
+//_getAllUserByOrgIDs 批量获取组织下所有用户
+func (U *UMS) _getAllUserByOrgIDs(orgIDs []uint64, pageSize uint64, page int) ([]UMSUser, uint64, error) {
+	url := fmt.Sprintf("%s/rs/organizations/query/orgs/users?pageNum=%d&pageSize=%d&productID=%d", UMSBusinessURL, page, pageSize, 20)
+	body, _ := json.Marshal(orgIDs)
+	statusCode, res, err := Request("POST", url, strings.NewReader(string(body)))
+	if err != nil {
+		return nil, 0, err
+	}
+	logs.Debug("_getAllUserByOrgIDs url:", url, "body:", string(body), "code:", statusCode)
+	var data struct {
+		RetCode uint64 `json:"retCode"`
+		RetMsg  string `json:"retMsg"`
+		RetObj  struct {
+			TotalCount uint64    `json:"totalCount"`
+			UserList   []UMSUser `json:"dataSet"`
+		} `json:"retObj"`
+	}
+	if err := json.Unmarshal(res, &data); err != nil {
+		return nil, 0, err
+	}
+	return data.RetObj.UserList, data.RetObj.TotalCount, nil
 }
