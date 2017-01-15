4bdb331543841c06e6a6568337ac4f2782a41ca8
guanzheng <zheng.guan@gnetoffice.com>
2017-01-15 20:18:33 +0800

    update

--- application/library/hbase/hbase_test.go
+++ application/library/hbase/hbase_test.go
@@ -21,10 +21,31 @@ func TestHbasePut(t *testing.T) {
 			&TColumnValue{
 				Family:    []byte("data"),
 				Qualifier: []byte("feed"),
-				Value:     []byte("ddddd"),
+				Value:     []byte("ddddd32"),
 			},
 		},
 	}
+	if err := client.Put([]byte("news"), TPut); err != nil {
+		t.Error(err)
+	}
+}
 
-	client.Put([]byte("bbs_feed"), TPut)
+func TestHbaseDel(t *testing.T) {
+	initHbase()
+	client, _ := OpenClient()
+	defer CloseClient(client)
+	tdel := &TDelete{
+		Row: []byte("ddddd"),
+		// Columns: []*TColumn{
+		// 	&TColumn{
+		// 		Family:    []byte("data"),
+		// 		Qualifier: []byte("feed"),
+		// 		//	Timestamp:
+		// 	},
+		// },
+		DeleteType: TDeleteType_DELETE_COLUMN,
+	}
+	if err := client.DeleteSingle([]byte("news"), tdel); err != nil {
+		t.Error(err)
+	}
 }
