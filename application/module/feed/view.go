76a25a664f9d6cc178b37158465a6c1c7aa69a2b
guanzheng <zheng.guan@gnetoffice.com>
2017-07-17 11:00:54 +0800

    update

--- application/module/feed/view.go
+++ application/module/feed/view.go
@@ -10,9 +10,9 @@ import (
 
 //GetBbsView 图文广播模板
 func GetBbsView(data *Bbs) string {
-	description := data.BoardName
-	if data.Description != "" {
-		description = data.Description
+	description := data.Description
+	if data.DiscussID == 0 && data.Description == "" {
+		description = data.BoardName
 	}
 	return `<div style="font-family:PingFangSC-Medium,Microsoft YaHei,Arial,serif;font-size:14px;padding:10px;box-sizing:border-box;text-align:left;" class="bbs"><div style="padding-bottom:5px;"><span style="display:inline-block;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;max-width:100%;">` + data.Title + `</span></div><div style="text-align:center;border-radius:3px;overflow:hidden;height:0;padding-bottom:56.112%"><img src="` + data.Thumb + `" style="width:100%"></div><div style="overflow:hidden;-webkit-line-clamp:2;padding:0 10px;line-height:1.5;text-align:left;height:36px;font-size:12px;color:#666;word-wrap:break-word;text-overflow:ellipsis;margin-top:10px;display:-webkit-box;-webkit-box-orient:vertical;">` + description + `</div></div>`
 }
